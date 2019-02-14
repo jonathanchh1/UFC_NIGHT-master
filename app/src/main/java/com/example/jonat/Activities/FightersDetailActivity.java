5f5eeb841d26ac85da9870ddb3a12592eb4b3b8a
jonathanchh1 <jonathanchh1@gmail.com>
2019-02-13 20:25:42 -0500

    updating adapters

---
+++ app/src/main/java/com/example/jonat/Activities/FightersDetailActivity.java
@@ -0,0 +1,15 @@
+package com.example.jonat.Activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.jonat.services.R;
+
+public class FightersDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fighters_detail);
+    }
+}
