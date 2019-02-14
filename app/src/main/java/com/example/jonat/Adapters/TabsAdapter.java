7bb3eadf8866ac1adbe2f21cf4279a4454f507e4
jonathanchh1 <jonathanchh1@gmail.com>
2019-02-14 10:24:54 -0500

    updating adapters

---
+++ app/src/main/java/com/example/jonat/Adapters/TabsAdapter.java
@@ -0,0 +1,42 @@
+package com.example.jonat.Adapters;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by jonat on 9/8/2016.
+ */
+public class TabsAdapter extends FragmentPagerAdapter {
+
+    private final List<Fragment> FragmentList = new ArrayList();
+    private final List<String> FragmentTitles = new ArrayList();
+
+    public TabsAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    public void addFragment(Fragment fragment, String title) {
+        FragmentList.add(fragment);
+        FragmentTitles.add(title);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return FragmentList.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return FragmentList.size();
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return FragmentTitles.get(position);
+    }
+
+}
