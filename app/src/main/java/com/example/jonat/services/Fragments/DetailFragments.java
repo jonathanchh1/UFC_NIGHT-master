adf343aa32281db197f56804db5dd29aef3507bd
jonathanchh1 <jonathanchh1@gmail.com>
2019-04-04 18:57:32 -0400

    update fragments

---
+++ app/src/main/java/com/example/jonat/services/Fragments/DetailFragments.java
@@ -0,0 +1,12 @@
+package com.example.jonat.services.Fragments;
+
+import android.app.Fragment;
+
+/**
+ * Created by jonat on 10/11/2017.
+ */
+
+public class DetailFragments extends Fragment{
+    public static final String Args = "arguments";
+
+}
